294a726e42b1691bfb295c8069db11e0cf858b23
Badal Kumar <bk8262@gmail.com>
2024-05-14 18:27:27 +0530

    Init Commit

--- Spring RestAPI/06.DataValidation-Input-and-url-Data/src/main/java/com/rest/repository/FriendFamilyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rest.repository;
-
-import com.rest.entity.FriendFamily;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FriendFamilyRepository extends CrudRepository<FriendFamily,Integer> {
-}
